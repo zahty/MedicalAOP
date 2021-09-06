ecef30f113254e2fde09f0d742a47321977ed3f9
Amin <ahmedamin.seth@yahoo.fr>
2021-09-06 19:02:03 +0100

    petite maj

--- src/main/java/com/m2i/demomedical/controller/PatientController.java
+++ src/main/java/com/m2i/demomedical/controller/PatientController.java
@@ -2,14 +2,16 @@ package com.m2i.demomedical.controller;
 
 import com.m2i.demomedical.entities.PatientEntity;
 import com.m2i.demomedical.service.PatientService;
+import com.m2i.demomedical.service.VilleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/patient")
@@ -18,8 +20,12 @@ public class PatientController {
     @Autowired
     final private PatientService ps;
 
-    public PatientController(PatientService ps) {
+    @Autowired
+    final private VilleService vs;
+
+    public PatientController(PatientService ps, VilleService vs) {
         this.ps = ps;
+        this.vs = vs;
     }
 
     @GetMapping("")
@@ -28,17 +34,61 @@ public class PatientController {
         return "patient/list";
     }
 
-
     @GetMapping("/add")
     public String add( Model model ){
-        //model.addAttribute( "lp" , ps.getList() );
+        model.addAttribute( "entete_titre" , "Ajouter un patient" );
+        model.addAttribute( "liste_villes" , vs.getList() );
+        model.addAttribute( "p" , new PatientEntity());
         return "patient/add_edit";
+    }
+
+    @PostMapping("/add")
+    public String addPost( HttpServletRequest request ){
+        try{
+            String nom = request.getParameter("nom");
+            String prenom = request.getParameter("prenom");
+            String telephone = request.getParameter("telephone");
+            String email = request.getParameter("email");
+            String ville = request.getParameter("ville");
+            ps.addPatient(nom, prenom, telephone, email, Integer.parseInt( ville ) );
+            return "redirect:/patient?success";
+        }catch( Exception e ){
+            return "patient/add_edit?error="+e.getMessage();
+        }
     }
 
     @GetMapping("/edit/{id}")
     public String edit( Model model , @PathVariable int id ){
-        model.addAttribute( "lp" , ps.find(id) );
+        model.addAttribute( "entete_titre" , "Editer un patient" );
+        model.addAttribute( "liste_villes" , vs.getList() );
+        model.addAttribute( "p" , ps.find(id) );
         return "patient/add_edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editPost( HttpServletRequest request , @PathVariable int id ){
+        try{
+            String nom = request.getParameter("nom");
+            String prenom = request.getParameter("prenom");
+            String telephone = request.getParameter("telephone");
+            String email = request.getParameter("email");
+            String ville = request.getParameter("ville");
+
+            ps.editPatient(id, nom, prenom, email, telephone , Integer.parseInt( ville ) );
+            return "redirect:/patient?success";
+        }catch( Exception e ){
+            return "patient/add_edit?error="+e.getMessage();
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete( @PathVariable int id ){
+        try{
+            ps.delete(id);
+            return "redirect:/patient?success";
+        }catch( Exception e ){
+        return "patient?error="+e.getMessage();
+        }
     }
 
 }
--- src/main/java/com/m2i/demomedical/service/PatientService.java
+++ src/main/java/com/m2i/demomedical/service/PatientService.java
@@ -3,11 +3,13 @@ package com.m2i.demomedical.service;
 import com.m2i.demomedical.entities.PatientEntity;
 import com.m2i.demomedical.entities.VilleEntity;
 import com.m2i.demomedical.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
 
+    @Autowired
     private PatientRepository pr;
 
     public PatientService(PatientRepository pr) {
@@ -18,29 +20,44 @@ public class PatientService {
         return pr.findAll();
     }
 
-    public PatientEntity addPatient(String nom, String prenom, String email, String telephone , int ville ){
+    public void addPatient(String nom, String prenom, String telephone , String email,  int ville ){
         try{
             PatientEntity p = new PatientEntity();
             p.setNom(nom);
             p.setPrenom(prenom);
             p.setEmail(email);
             p.setTelephone(telephone);
-
             VilleEntity villeP = new VilleEntity();
             villeP.setId( ville );
             p.setVille( villeP );
-
             pr.save( p );
-            return  p;
 
         }catch( Exception e ){
-
+            throw e;
         }
+    }
 
-        return null;
+    public void editPatient(int idp, String nom, String prenom, String email, String telephone , int ville ){
+        try{
+            PatientEntity p = pr.findById(idp).get();
+            p.setNom(nom);
+            p.setPrenom(prenom);
+            p.setEmail(email);
+            p.setTelephone(telephone);
+            VilleEntity villeP = new VilleEntity();
+            villeP.setId( ville );
+            p.setVille( villeP );
+            pr.save( p );
+        }catch( Exception e ){
+            throw e;
+        }
     }
 
     public PatientEntity find(int id) {
         return pr.findById( id ).get();
+    }
+
+    public void delete(int id) {
+        pr.deleteById(id);
     }
 }
